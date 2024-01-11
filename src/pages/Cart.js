90a4114086e3d4b074b522ff5200a1bf82695a60
martinromario55 <martinromario55@gmail.com>
2024-01-11 10:10:25 +0300

    dummy products commented out

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,23 +3,23 @@ import { useCart } from '../context/CartContext'
 import { useTitle } from '../hooks/useTitle'
 
 export const Cart = () => {
-  const {total, cartList} = useCart()
+  const { total, cartList } = useCart()
 
   useTitle('Cart')
-  const products = [
-    {
-      id: 1,
-      name: 'Sony Wh-Ch510 Bluetooth Wireless',
-      price: 149,
-      image: '/assets/images/1001.png',
-    },
-    {
-      id: 2,
-      name: 'boAt Rockerz 450',
-      price: 49,
-      image: '/assets/images/1002.png',
-    },
-  ]
+  // const products = [
+  //   {
+  //     id: 1,
+  //     name: 'Sony Wh-Ch510 Bluetooth Wireless',
+  //     price: 149,
+  //     image: '/assets/images/1001.png',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'boAt Rockerz 450',
+  //     price: 49,
+  //     image: '/assets/images/1002.png',
+  //   },
+  // ]
 
   return (
     <main>
